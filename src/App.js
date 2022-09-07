d352fc376255ac5cd6b41857d340159938cf5a6d
SOPHIN SHRESTHA <sophinstha123@gmail.com>
2022-09-07 21:34:22 +1000

    First Challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-box">
+      <div className="outer-box">
+        <img alt="QRCode" src={require("./assets/image-qr-code.png")} />
+        <div className="textField">
+          <h5>Improve your front-end skills by building projects</h5>
+          <p>
+            Scan the QR Code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
